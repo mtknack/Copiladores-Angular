a1388f89ad4c5c0c75b85afc343fe0f6f69c9d20
Dmantuan <daniel2mantuan@gmail.com>
2023-10-17 17:00:22 -0300

    Add tipo String como reservado, verificacao se e um numero

--- src/app/services/AnalizadorLexico.ts
+++ src/app/services/AnalizadorLexico.ts
@@ -3,91 +3,97 @@ import { Reservadas } from './Reservadas';
 import { Identificadores } from './Identificadores';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnalizadorLexico {
+  reservadas: Reservadas;
+  vetorDeToekens: [string] = [''];
+  identificadores: Identificadores;
 
-    reservadas: Reservadas;
-    vetorDeToekens: [string] = ['']
-    identificadores: Identificadores
+  constructor(reservadas: Reservadas, identificadores: Identificadores) {
+    this.reservadas = reservadas;
+    this.identificadores = identificadores;
+  }
 
-    constructor(
-        reservadas: Reservadas,
-        identificadores: Identificadores
-    ) {
-        this.reservadas = reservadas;
-        this.identificadores = identificadores
-    }
+  private formatadorDeTexto(texto: string[]): string[] {
+    // fazer a parte de formatar texto aqui
+    return texto;
+  }
 
-    private formatadorDeTexto(texto: string[]): string[] {
+  private processarTexto(texto: string): string {
+    // Ignorar linhas que começam com // (com espaços antes ou depois)
+    texto = texto.replace(/^\s*\/\/.*$/gm, '');
 
-        // fazer a parte de formatar texto aqui
-        return texto
-    }
+    // Ignorar blocos de comentários /* ... */ (incluindo espaços dentro dos delimitadores)
+    texto = texto.replace(/\/\*[\s\S]*?\*\//g, '');
 
-    private processarTexto(texto: string): string {
-        // Ignorar linhas que começam com // (com espaços antes ou depois)
-        texto = texto.replace(/^\s*\/\/.*$/gm, '');
+    return texto;
+  }
 
-        // Ignorar blocos de comentários /* ... */ (incluindo espaços dentro dos delimitadores)
-        texto = texto.replace(/\/\*[\s\S]*?\*\//g, '');
+  private separarTexto(texto: string): string[] {
+    var textoSemComentario = this.processarTexto(texto);
+    var vetorTexto = textoSemComentario
+      .split(/\s+/)
+      .filter((palavra) => palavra !== '');
 
-        return texto;
-    }
+    console.log(vetorTexto);
 
-    private separarTexto(texto: string): string[] {
+    let linhas: string[] = [];
+    vetorTexto.forEach((x) => {
+      if (
+        x.includes('(') ||
+        x.includes(')') ||
+        x.includes('{') ||
+        x.includes('}') ||
+        x.includes('[') ||
+        x.includes(']')
+      ) {
+        let substrings = x
+          .split(/([()\[\]])/)
+          .map((substring) => substring.trim())
+          .filter((substring) => substring !== '');
+        linhas.push(...substrings);
+      } else {
+        linhas.push(x);
+      }
+    });
 
-        var textoSemComentario = this.processarTexto(texto);
-        var vetorTexto = textoSemComentario.split(/\s+/).filter(palavra => palavra !== '');
+    vetorTexto = linhas;
 
-        console.log(vetorTexto)
+    return vetorTexto;
+  }
 
-        let linhas: string[] = [];
-        vetorTexto.forEach(x => {
-            if (
-                x.includes('(')
-                || x.includes(')')
-                || x.includes('{')
-                || x.includes('}')
-                || x.includes('[')
-                || x.includes(']')
-            ) {
-                let substrings = x.split(/([()\[\]])/).map(substring => substring.trim()).filter(substring => substring !== '');
-                linhas.push(...substrings);
-            } else {
-                linhas.push(x);
-            }
-        });
+  public analizadorLexico(texto: string): string[] {
+    const textoVetor = this.separarTexto(texto);
+    this.vetorDeToekens = [''];
 
-        vetorTexto = linhas
+    var i = 0;
+    textoVetor.map((palavra) => {
+      this.vetorDeToekens[i] = this.reservadas.buscaReservadas(palavra);
+      //Verifica se e um identificador
+      if (
+        this.vetorDeToekens[i] == 'N/A' &&
+        this.identificadores.VerificarIdentificador(palavra)
+      ) {
+        this.vetorDeToekens[i] = palavra;
+      }
+      //Verfica se e uma string
+      else if (/^".*"$/.test(palavra) || /^'.*'$/.test(palavra)) {
+        this.vetorDeToekens[i] = palavra;
+      }
+      //Verifica se e um numero
+      else if (/^-?\d+(\.\d+)?$/.test(palavra)) {
+        this.vetorDeToekens[i] = palavra;
+      }
+      //Se nao e nada e um identificador invalido
+      else if (this.vetorDeToekens[i] == 'N/A') {
+        this.vetorDeToekens[i] = 'IdetificadorInvalido';
+      }
 
-        return vetorTexto
-    }
+      i++;
+    });
 
-    public analizadorLexico(texto: string): string[] {
-
-        const textoVetor = this.separarTexto(texto)
-        this.vetorDeToekens = [''];
-
-        var i = 0
-        textoVetor.map(palavra => {
-            
-            this.vetorDeToekens[i] = this.reservadas.buscaReservadas(palavra)
-            if (this.vetorDeToekens[i] == 'N/A' && this.identificadores.VerificarIdentificador(palavra)) {
-                this.vetorDeToekens[i] = palavra
-            }
-            else if (/^".*"$/.test(palavra)) {
-                this.vetorDeToekens[i] = palavra;
-            }
-            else if (this.vetorDeToekens[i] == 'N/A') {
-                this.vetorDeToekens[i] = 'IdetificadorInvalido'
-            }
-
-            i++
-        })
-
-        console.log(this.vetorDeToekens)
-        return this.formatadorDeTexto(this.vetorDeToekens)
-    }
-
+    console.log(this.vetorDeToekens);
+    return this.formatadorDeTexto(this.vetorDeToekens);
+  }
 }
--- src/app/services/Identificadores.ts
+++ src/app/services/Identificadores.ts
@@ -1,35 +1,26 @@
 import { Injectable } from '@angular/core';
 
 const regrasDeIdentificador = {
-    R1: /^[À-ÿa-zA-Z$_][À-ÿa-zA-Z$_0-9]*/,
-    R2: /^(?!true$|false$).*/i,
-    R3: /^(?!null$).*/i,
-    R4: /^(?!null$).*/i,
-    R5: /^[À-ÿa-zA-Z$_][À-ÿa-zA-Z$_0-9]*/
-}
+  R1: /^[À-ÿa-zA-Z$_][À-ÿa-zA-Z$_0-9]*/,
+  R2: /^(?!true$|false$).*/i,
+  R3: /^(?!null$).*/i,
+  R4: /^[À-ÿa-zA-Z$_][À-ÿa-zA-Z$_0-9]*/,
+};
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class Identificadores {
-
-    VerificarIdentificador(identificador: string): boolean{
-
-        if(
-            !regrasDeIdentificador.R1.test(identificador)
-            ||
-            !regrasDeIdentificador.R2.test(identificador)
-            ||
-            !regrasDeIdentificador.R3.test(identificador)
-            ||
-            !regrasDeIdentificador.R4.test(identificador)
-            ||
-            !regrasDeIdentificador.R5.test(identificador)
-        ){
-            return false
-        }
-    
-        return true;
+  VerificarIdentificador(identificador: string): boolean {
+    if (
+      !regrasDeIdentificador.R1.test(identificador) ||
+      !regrasDeIdentificador.R2.test(identificador) ||
+      !regrasDeIdentificador.R3.test(identificador) ||
+      !regrasDeIdentificador.R4.test(identificador)
+    ) {
+      return false;
     }
 
+    return true;
+  }
 }
