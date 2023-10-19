247058bea21499de73e74ce9ec482eae5257cb5a
Dmantuan <daniel2mantuan@gmail.com>
2023-10-18 21:56:11 -0300

    Acertando validacao de acento

--- src/app/services/Identificadores.ts
+++ src/app/services/Identificadores.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 
 const regrasDeIdentificador = {
-  R1: /^[À-ÿa-zA-Z$_][À-ÿa-zA-Z$_0-9]*/,
+  R1: /^[a-zA-Z$_][a-zA-Z$_0-9]*/,
   R2: /^(?!true$|false$).*/i,
   R3: /^(?!null$).*/i,
-  R4: /^[À-ÿa-zA-Z$_][À-ÿa-zA-Z$_0-9]*/,
 };
 
 @Injectable({
@@ -15,8 +14,7 @@ export class Identificadores {
     if (
       !regrasDeIdentificador.R1.test(identificador) ||
       !regrasDeIdentificador.R2.test(identificador) ||
-      !regrasDeIdentificador.R3.test(identificador) ||
-      !regrasDeIdentificador.R4.test(identificador)
+      !regrasDeIdentificador.R3.test(identificador)
     ) {
       return false;
     }
